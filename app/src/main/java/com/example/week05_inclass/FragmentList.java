4ba1bc2e1bcdf79a96bec34b1a36e6f14469e4b2
21120076 <107020295+hfkhdslgsfs@users.noreply.github.com>
2023-10-19 08:12:41 +0700

    Custom button

--- app/src/main/java/com/example/week05_inclass/FragmentList.java
+++ app/src/main/java/com/example/week05_inclass/FragmentList.java
@@ -26,7 +26,7 @@ public class FragmentList extends Fragment {
     Context context;
     String id[] = {"21120185", "21120182","21120176","21120099","21120076","21120075","21120074","21120073","21120072","21120071","21120070","21120069","21120068","21120067","21120066","21120065","21120064","21120063","21120062","21120061"};
     String fullName[] = {"Phạm Vân Anh Thư", "Phan Trí Nhân","Đinh Thị Thúy Hường","Hoàng Thành Nam","Nguyễn Thanh Huệ","Nguyễn Thị Hoa","Trần Kim Liên","Đỗ Ngọc Hà","Nguyễn Thùy Chi","Trần Ngọc Diễm","Lê Thị Bích Hồng","Lê Như Thảo","Lý Thanh Ngân","Lê Nhật Nam","Lê Phương Thùy","Trần Tiểu Vy","Lê Hữu Nghĩa","Nguyễn Hoàng Mai","Lê Như Ý","Võ Ý Nhi"};
-    String points[] = {"10", "9","9","9","10","6","7","8","9","10","1", "2","3","4","5","6","7","8","9","10"};
+    String points[] = {"10", "9","9","9","10","6","7","8","9","10","5", "6","8","8","5","6","7","8","9","10"};
     Integer[] icons={R.drawable.actor_1,R.drawable.actor_2,R.drawable.actor_3,R.drawable.actor_4,R.drawable.actor_5,R.drawable.actor_6,R.drawable.actor_7,R.drawable.actor_8,R.drawable.actor_9,R.drawable.actor_10,R.drawable.actor_11,R.drawable.actor_12,R.drawable.actor_13,R.drawable.actor_14,R.drawable.actor_15,R.drawable.actor_16,R.drawable.actor_17,R.drawable.actor_18,R.drawable.actor_19,R.drawable.actor_20};
 
     String classID="21CTT2";
@@ -74,12 +74,24 @@ public class FragmentList extends Fragment {
                 if (position==0){
                     main.onMsgFromFragToMain("list-frag-first",id[position],fullName[position],classID,points[position]);
                 }
-                
+
+                setBackgroundItem(position);
+
                 main.onMsgFromFragToMain("list-frag",id[position],fullName[position],classID,points[position]);
                 txtChoosen.setText("Mã số: "+id[position]);
             }
         });
         return layout_list;
+    }
+
+    public void setBackgroundItem(int position){
+        listView.getChildAt(position).setBackgroundColor(Color.parseColor("#00ced1"));
+
+//        for(int i =0; i< id.length; i++){
+//            if(i!=position){
+//                listView.getChildAt(i).setBackgroundColor(Color.parseColor("#ffccddff"));
+//            }
+//        }
     }
 
     public void onMsgFromMainToFragment(int position) {
